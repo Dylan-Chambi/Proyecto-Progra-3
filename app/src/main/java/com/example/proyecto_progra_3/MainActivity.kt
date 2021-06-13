30f638baf7ff6cc5954626cf3f7a944ec213b1ce
= <=>
2021-06-12 22:05:37 -0400

    Segunda pantalla xml

--- app/src/main/java/com/example/proyecto_progra_3/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_progra_3/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.proyecto_progra_3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val loginButton = findViewById<Button>(R.id.IniciarSesion)
+
+        loginButton.setOnClickListener{
+            val intent = Intent(this, LoginScreen::class.java)
+            startActivity(intent)
+        }
     }
 }
