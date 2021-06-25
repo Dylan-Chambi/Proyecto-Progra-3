173e71db5092c86712e9d302c0dbe0e7d4ee2abd
dylan <dylanchambifi@gmail.com>
2021-06-24 22:23:13 -0400

    funcionamiento de la pantalla de inicio de sesion con la base de datos de Firebase

--- app/src/main/java/com/example/proyecto_progra_3/LoginScreen.kt
+++ app/src/main/java/com/example/proyecto_progra_3/LoginScreen.kt
@@ -1,43 +1,85 @@
 package com.example.proyecto_progra_3
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.*
-import java.util.*
+import com.example.proyecto_progra_3.databinding.ActivityLoginScreenBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 class LoginScreen : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginScreenBinding
+    private lateinit var progressBar: ProgressBar
+    private lateinit var auth: FirebaseAuth
+    lateinit var enterButton: Button
+    lateinit var userEmail: EditText
+    lateinit var userPassword: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_screen)
+        /**
         val usuario1 = Usuario("Fer123", "Fernando Alvarado Lanza", 77716326, "Pass123")
         val usuario2 = Usuario("Salao", "Samuel Escobar Bejarano", 77585417, "pbbs159")
         val usuario3 = Usuario("Pary", "Luis Andres Paricollo Parra", 77585417, "samusalao")
         val usuario4 = Usuario("", "Dylan Chambi Frontanilla", 0, "")
-        val listaUsuarios = mutableListOf(usuario1, usuario2, usuario3, usuario4)
-        val botonEntrar = findViewById<Button>(R.id.botonEntrar)
-        val nombreDeUsuario = findViewById<EditText>(R.id.nombreDeUsuario)
-        val contrasenia = findViewById<EditText>(R.id.contrasenia)
-        botonEntrar.setOnClickListener{
-            var usuarioEncontrado = false
-            listaUsuarios.forEach {
-                if (nombreDeUsuario.text.toString() == it.nombreUsuario && contrasenia.text.toString() == it.contrasenia) {
-                    usuarioEncontrado = true
-                    val intent = Intent(this, Menu::class.java)
-                    finish()
+        */
+        binding = ActivityLoginScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = Firebase.auth
+
+        progressBar = findViewById(R.id.progressBarLogin)
+        enterButton = findViewById(R.id.botonEntrar)
+        userEmail = findViewById(R.id.nombreDeUsuario)
+        userPassword = findViewById(R.id.contrasenia)
+        enterButton.setOnClickListener{
+
+        }
+        binding.botonEntrar.setOnClickListener {
+            progressBar.visibility = View.VISIBLE
+            val email = userEmail.text.toString()
+            val password = userPassword.text.toString()
+            when{
+                email.isEmpty() || password.isEmpty() -> {
+                    showShortMessage(this, "Authentication failed.")
+                    val intent = Intent(this, LoginScreen::class.java)
                     startActivity(intent)
+                    finish()
+                } else -> {
+                signIn(email, password)
                 }
             }
-            if (!usuarioEncontrado){
-            Toast.makeText(
-                this,
-                "Nombre de Usuario o Contraseña Incorrectos Porfavor Intentelo de Nuevo",
-                Toast.LENGTH_LONG
-            ).show()
-            }
         }
+    }
+
+
+    private fun signIn(userEmail: String, userPassword: String){
+        auth.signInWithEmailAndPassword(userEmail, userPassword)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    showShortMessage(baseContext, "Authentication success.")
+                    val intent = Intent(this, Menu::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    showShortMessage(baseContext, "Authentication failed.")
+                    val intent = Intent(this, LoginScreen::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
     }
 
 }
