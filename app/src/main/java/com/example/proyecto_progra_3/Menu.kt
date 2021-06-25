457163c67b18bece79c23e5607da027e916dd1b7
dylan <dylanchambifi@gmail.com>
2021-06-25 05:36:31 -0400

    añadido un boton de cerrar sesion provisional

--- app/src/main/java/com/example/proyecto_progra_3/Menu.kt
+++ app/src/main/java/com/example/proyecto_progra_3/Menu.kt
@@ -3,17 +3,31 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import com.example.proyecto_progra_3.databinding.ActivityLoginScreenBinding
+import com.example.proyecto_progra_3.databinding.ActivityMenuBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class Menu : AppCompatActivity() {
+
+    private lateinit var bindingMenu: ActivityMenuBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        bindingMenu = ActivityMenuBinding.inflate(layoutInflater)
+        setContentView(bindingMenu.root)
+        auth = FirebaseAuth.getInstance()
 
         val farmaciasButton = findViewById<ImageView>(R.id.farmacias)
         val centrosMEdicosButton = findViewById<ImageView>(R.id.cruzRoja)
         val ambulanciaButton = findViewById<ImageView>(R.id.ambulancia)
         val guiasButton = findViewById<ImageView>(R.id.libro)
 
+        bindingMenu.logOutButton.setOnClickListener {
+            logOut()
+        }
         farmaciasButton.setOnClickListener{
             val intent = Intent(this, SearchProductActivity::class.java)
             startActivity(intent)
@@ -31,5 +45,12 @@ class Menu : AppCompatActivity() {
             startActivity(intent)
         }
 
+    }
+    private fun logOut(){
+        auth.signOut()
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        showLongMessage(this, "Log Out successfully")
+        finish()
     }
 }
